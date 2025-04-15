36a2a9952630af1ff23cf8130d43b5e313d1a63b
Yael Aharon <yael.aharon.m@gmail.com>
2025-04-15 17:55:44 -0400

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+
+const Homepage = async () => {
+  const latestProducts = await getLatestProducts();
+  return (
+    <>
+      <ProductList data={latestProducts} title="Newest Arrivals" />
+    </>
+  );
+};
+
+export default Homepage;
---
+++ lib/actions/product.actions.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { prisma } from "./../../db/prisma";
+import { LATEST_PRODUCTS_LIMIT } from "../constants";
+import { convertToPlainObject } from "../utils";
+
+export async function getLatestProducts() {
+  const data = await prisma.product.findMany({
+    take: LATEST_PRODUCTS_LIMIT,
+    orderBy: { createdAt: "desc" },
+  });
+  return convertToPlainObject(data);
+}
+
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+}
